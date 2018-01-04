c9d51069a56bec760ba2a01892e4077818678604
Thomas King <thomasking.gs@outlook.com>
2018-01-04 10:23:32 -0500

    homework

---
+++ day3_homework.rb
@@ -0,0 +1,18 @@
+def add(num1, num2)
+	return num1 + num2
+end
+
+def sub(num1, num2)
+	return num1 - num2
+end
+
+def div(num1, num2)
+	return num1 / num2
+end
+
+def mul(num1, num2)
+	return num1 * num2
+end
+
+def 
+
